eeeb132435b843f05d7d46de92da3a6f465876e4
andresvaldezt <andresvaldezt@gmail.com>
2026-03-08 21:03:55 -0600

    Add NotFoundPage component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,12 @@
 export default function NotFoundPage(){
     return(
         <main>
-            <h1>404 - Pagina no encontrada</h1>
-            <p>Lo sentimos, la pagina uqe buscas no existe.</p>
+            <div className="flex flex-col gap-12 mx-auto px-4 py-6 sm:px-6 md:px-8 max-w-7xl">
+                <section className="error grow flex flex-col gap-6 rounded-md px-3 py-2 font-medium bg-gray-700">
+                    <h1 className="text-4xl">Error 404 - Page Not Found</h1>
+                    <p>Sorry, an error has occurred, the page you're looking for doesn't exist.</p>
+                </section>
+            </div>
         </main>
     )
 }
