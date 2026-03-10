376b9b2d45d9af99f2af896e23b803366849f064
andresvaldezt <andresvaldezt@gmail.com>
2026-03-10 17:13:03 -0600

    Refactor styles in App, Header, and Home components; improve theme handling and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import { IconCodeCircle2, IconMoonStars, IconSun  } from '@tabler/icons-react';
 import { useState } from 'react';
 
 export function Header(){
+    const linkStyles = 'rounded-md px-3 py-2 font-medium hover:shadow-lg/30 shadow-white hover:text-amber-400 light:hover:shadow-lg/30 light:shadow-black light:hover:text-purple-400 transition-all ease-in-out'
 
     return(
-        <header className="port-header shadow-lg/10 shadow-white light:shadow-gray-800 text-white light:text-black">
+        <header className="port-header sticky top-0 z-50 mb-10 shadow-lg/10 bg-black light:bg-white shadow-white light:shadow-black text-white light:text-black">
             <div className='flex justify-between items-center px-6 w-full max-w-7xl mx-auto'>
                 <div>
                     <a href="/" className='no-underline'>
@@ -16,10 +17,10 @@ export function Header(){
                 </div>
                 <div>
                     <nav className='hidden lg:flex items-center ms-auto no-underline'>
-                        <a className='rounded-md px-3 py-2 font-medium hover:bg-gray-700 hover:text-white' href="/">About</a>
-                        <a className='rounded-md px-3 py-2 font-medium hover:bg-gray-700 hover:text-white' href="/">Work Experience</a>
-                        <a className='rounded-md px-3 py-2 font-medium hover:bg-gray-700 hover:text-white' href="/">Projects</a>
-                        <a className='rounded-md px-3 py-2 font-medium hover:bg-gray-700 hover:text-white' href="/">Resume</a>
+                        <a className={linkStyles} href="/">About</a>
+                        <a className={linkStyles} href="/">Work Experience</a>
+                        <a className={linkStyles} href="/">Projects</a>
+                        <a className={linkStyles} href="/">Resume</a>
                         |
                         <ThemeButton></ThemeButton>
                     </nav>
@@ -39,7 +40,7 @@ const ThemeButton = () => {
     }
 
     return(
-        <button onClick={toggleTheme} className="rounded-full p-2 shadow-lg/30 hover:shadow-lg/50 shadow-amber-100 hover:shadow-amber-400 hover:text-amber-400 light:shadow-gray-400 light:text-black light:hover:shadow-purple-400 cursor-pointer light:hover:text-purple-400 transition-all ease-in-out">
+        <button onClick={toggleTheme} className="rounded-full p-2  shadow-lg/30 hover:shadow-lg/50 shadow-amber-100 hover:shadow-amber-400 hover:text-amber-400 light:shadow-gray-400 light:text-black light:hover:shadow-purple-400 cursor-pointer light:hover:text-purple-400 transition-all ease-in-out">
             {theme === 'dark' ? <IconSun stroke={2} /> : <IconMoonStars stroke={2} />}
         </button>
     ) 
